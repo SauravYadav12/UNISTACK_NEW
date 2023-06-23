c339b33b72d7480e36b105397327f2b77b9107eb
System Administrator <root@Sauravs-MacBook-Pro.local>
2023-06-24 01:57:19 +0530

    save with hardkey

--- public/js/hardKeySubmit.js
+++ public/js/hardKeySubmit.js
@@ -2,9 +2,10 @@ $(document).ready(function(){
     //ctrl+s
     $(document).unbind('keypress');
     $(document).keydown(function(event) {
-        event.preventDefault();
+        
         console.log(event);
         if ((event.which == 115 || event.which == 83) && (event.ctrlKey||event.metaKey)|| (event.which == 19)) {
+            event.preventDefault();
             $("form").submit();
             console.log("Form submitted")
             return false;
